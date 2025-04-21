9987da1d4144f567dbab2506485e8d77922e8646
caiquemiranda <caiquemirandas@gmail.com>
2025-04-20 23:51:17 -0300

    Refactor criarFaixas function to encapsulate logic for creating faixa elements

--- js/rua.js
+++ js/rua.js
@@ -1,9 +1,13 @@
-const elementoComFaixas = document.querySelectorAll('[faixas]');
-elementoComFaixas.forEach((el) => {
-    const qtd = +el.getAttribute('faixas')
-    for (let i = 0; i < qtd; i++) {
-        const faixa = document.createElement('div')
-        faixa.classList.add('faixa')
-        el.appendChild(faixa)
-    }
-})
+function criarFaixas() {
+    const elementoComFaixas = document.querySelectorAll('[faixas]');
+    elementoComFaixas.forEach((el) => {
+        const qtd = +el.getAttribute('faixas')
+        for (let i = 0; i < qtd; i++) {
+            const faixa = document.createElement('div')
+            faixa.classList.add('faixa')
+            el.appendChild(faixa)
+        }
+    })
+}
+
+criarFaixas()

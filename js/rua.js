25bdc7629a3ca28188bc2898c9461f1ab9abecfb
caiquemiranda <caiquemirandas@gmail.com>
2025-04-20 23:49:45 -0300

    Add initial HTML, CSS, and JavaScript files for elevator project

---
+++ js/rua.js
@@ -0,0 +1,9 @@
+const elementoComFaixas = document.querySelectorAll('[faixas]');
+elementoComFaixas.forEach((el) => {
+    const qtd = +el.getAttribute('faixas')
+    for (let i = 0; i < qtd; i++) {
+        const faixa = document.createElement('div')
+        faixa.classList.add('faixa')
+        el.appendChild(faixa)
+    }
+})

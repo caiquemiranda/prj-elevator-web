6417794934bf87a7bb9713a1f9be025bdf7fdce6
caiquemiranda <caiquemirandas@gmail.com>
2025-04-20 23:52:51 -0300

    Wrap criarFaixas function in an IIFE for better scope management

--- js/rua.js
+++ js/rua.js
@@ -1,13 +1,15 @@
-function criarFaixas() {
-    const elementoComFaixas = document.querySelectorAll('[faixas]');
-    elementoComFaixas.forEach((el) => {
-        const qtd = +el.getAttribute('faixas')
-        for (let i = 0; i < qtd; i++) {
-            const faixa = document.createElement('div')
-            faixa.classList.add('faixa')
-            el.appendChild(faixa)
-        }
-    })
-}
-
-criarFaixas()
+(function() {
+    function criarFaixas() {
+        const elementoComFaixas = document.querySelectorAll('[faixas]');
+        elementoComFaixas.forEach((el) => {
+            const qtd = +el.getAttribute('faixas')
+            for (let i = 0; i < qtd; i++) {
+                const faixa = document.createElement('div')
+                faixa.classList.add('faixa')
+                el.appendChild(faixa)
+            }
+        })
+    }
+    
+    criarFaixas()
+})()
